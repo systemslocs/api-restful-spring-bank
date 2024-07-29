de144a282bc70a8535f076864826e320faa36d2a
systemslocs <systemslocs@gmail.com>
2024-07-29 10:08:21 -0300

    api finalizada

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/api-restful-bank-0.0.1-SNAPSHOT.jar
--- src/main/java/me/dio/project/Application.java
+++ src/main/java/me/dio/project/Application.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.AbstractEnvironment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
-		SpringApplication.run(Application.class, args);
-	}
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "prd");
+        SpringApplication.run(Application.class, args);
+    }
 
 }
